2c6e18ceac1c6ee155ca8e48ea7f23272b591ca9
whuCanon <liu136583410@gmail.com>
2016-03-10 23:38:46 +0800

    Update homework_3

--- homework_3/MiniGUI.py
+++ homework_3/MiniGUI.py
@@ -3,6 +3,42 @@ import time
 
 
 class Canvas:
+    def __init__(self, width, height):
+        self.height = height
+        self.width = width
+        self.tupMatrix = {}
+
+    def draw(self):
+        tmp_str = ""
+        for i in range(self.height):
+            tmp_str = ""
+            for j in range(self.width):
+                if self.tupMatrix.has_key((i, j)):
+                    tmp_str += self.tupMatrix[(i, j)]
+                else:
+                    tmp_str += " "
+            print tmp_str
+
+    def draw_Line(self, text, pos):
+        cursor_pos = pos
+        for ch in text:
+            self.tupMatrix[tuple(cursor_pos)] = ch
+            cursor_pos[1] += 1
+
+    def clear(self):
+        self.tupMatrix.clear()
+
+    def update(self):
+        os.system('cls')
+        self.draw()
+        self.clear()
+        try:
+            time.sleep(self.REFRESH_RATE)
+        except KeyboardInterrupt:
+            pass
+
+
+class Tablet(Canvas):
     REFRESH_RATE = 0.05
     FONT_WIDTH = 9
     FONT_HEIGHT = 10
@@ -39,58 +75,24 @@ class Canvas:
     def __init__(self, rows, cols):
         self.rows = rows
         self.cols = cols
-        self.tupMatrix = {}
-
-    def draw(self):
-        tmp_str = ""
-        for i in range(self.rows):
-            tmp_str = ""
-            for j in range(self.cols):
-                if self.tupMatrix.has_key((i, j)):
-                    tmp_str += self.tupMatrix[(i, j)]
-                else:
-                    tmp_str += " "
-            print tmp_str
-
-    def draw_Line(self, text, pos):
-        cursor_pos = pos
-        for ch in text:
-            self.tupMatrix[tuple(cursor_pos)] = ch
-            cursor_pos[1] += 1
-
-    def clear(self):
-        self.tupMatrix.clear()
+        Canvas.__init__(self, cols * self.FONT_WIDTH + 1, rows * self.FONT_HEIGHT + 1)
 
     def update(self):
-        os.system('cls')
-        self.draw()
-        self.clear()
-        time.sleep(self.REFRESH_RATE)
-
-
-class Tablet(Canvas):
-
-    def __init__(self, rows, cols):
-        Canvas.__init__(self, rows, cols)
-
-    def update(self):
-        for i in range(self.rows / self.FONT_HEIGHT):
-            for j in range(self.cols):
+        for i in range(self.height / self.FONT_HEIGHT):
+            for j in range(self.width):
                 self.tupMatrix[((i + 1) * self.FONT_HEIGHT, j)] = '-'
-        for i in range(self.rows):
+        for i in range(self.height):
             self.tupMatrix[(i, 0)] = '|'
-            self.tupMatrix[(i, self.cols - 1)] = '|'
-        for i in range(self.cols):
+            self.tupMatrix[(i, self.width - 1)] = '|'
+        for i in range(self.width):
             self.tupMatrix[(0, i)] = '='
-            self.tupMatrix[(self.rows - 1, i)] = '='
+            self.tupMatrix[(self.height - 1, i)] = '='
         Canvas.update(self)
 
     def draw_Text(self, text, pos = [0, 0]):
         str_len = len(text)
-        char_rows = self.rows / self.FONT_HEIGHT
-        char_cols = self.cols / self.FONT_WIDTH
-        for i in range(char_rows):
-            for j in range(char_cols):
+        for i in range(self.rows):
+            for j in range(self.cols):
                 if str_len == 0:
                     break
                 try:
@@ -104,5 +106,5 @@ class Tablet(Canvas):
                             cursor_pos[1] = j * self.FONT_WIDTH + pos[1] + 1
                     str_len -= 1
                 except KeyError:
-                    self.draw_Line([1, 1], "only upper case!")
+                    self.draw_Line("only upper case!", [1, 1])
 
--- homework_3/homework_3.py
+++ homework_3/homework_3.py
@@ -4,18 +4,21 @@ import MiniGUI
 
 
 RUN_MODE = 1
+WRITE_ROWS = 2    # < 3
+WRITE_COLS = 10
+PAINT_WIDTH = 80
+PAINT_HEIGHT = 30
 
 
 def change_mode(mode):
-    global RUN_MODE
     if mode == '0':
-        RUN_MODE = 0
+        run_mode0()
     elif mode == '1':
-        RUN_MODE = 1
+        run_mode1()
     elif mode == '2':
-        RUN_MODE = 2
+        run_mode2()
     elif mode == '3':
-        RUN_MODE = 3
+        run_mode3()
 
 
 def run_mode0():
@@ -23,43 +26,46 @@ def run_mode0():
 
 
 def run_mode1():
-    MiniGUI.Canvas.REFRESH_RATE = 1
-    x.draw_Line("Welcome!", [1, x.cols * 0.45])
-    x.draw_Line("0.read the source code", [2, x.cols * 0.3])
-    x.draw_Line("1.homework_level1: here is", [3, x.cols * 0.3])
-    x.draw_Line("2.homework_level2: print any upper case on screen", [4, x.cols * 0.3])
-    x.draw_Line("3.homework_level3: let it be dynamic", [5, x.cols * 0.3])
-    x.draw_Line("Please enter 0-3 to select",[6, x.cols * 0.3])
-    x.draw_Text("LWT", [10, x.cols * 0.35])
-    x.update()
+    global canvas
+    canvas = MiniGUI.Tablet(WRITE_ROWS, WRITE_COLS)
+    canvas.draw_Line("Welcome!", [1, int(canvas.width * 0.45)])
+    canvas.draw_Line("0.read the source code", [2, int(canvas.width * 0.3)])
+    canvas.draw_Line("1.homework_level1: here is", [3, int(canvas.width * 0.3)])
+    canvas.draw_Line("2.homework_level2: print any upper case on screen", [4, int(canvas.width * 0.3)])
+    canvas.draw_Line("3.homework_level3: let it be dynamic", [5, int(canvas.width * 0.3)])
+    canvas.draw_Line("Please enter 0-3 to select", [7, int(canvas.width * 0.3)])
+    canvas.draw_Text("LWT", [10, int(canvas.width * 0.35)])
+    canvas.update()
     try:
-        change_mode(raw_input()[0])
-    except:
+        change_mode(raw_input("Enter the run_mode(0-3): ")[0])
+    except KeyboardInterrupt:
         pass
 
 
 def run_mode2():
-    pass
+    global canvas
+    canvas = MiniGUI.Tablet(WRITE_ROWS, WRITE_COLS)
+    try:
+        canvas.draw_Text(raw_input("Enter any number of upper case: ")[ : -1])
+    except KeyboardInterrupt:
+        pass
+    canvas.update()
+    try:
+        change_mode(raw_input("Enter the run_mode(0-3): ")[0])
+    except KeyboardInterrupt:
+        pass
 
 
 def run_mode3():
+    global canvas
     pass
 
 
 def run():
-    # main loop
-    while True:
-        if RUN_MODE == 0:
-            run_mode0()
-        elif RUN_MODE == 1:
-            run_mode1()
-        elif RUN_MODE == 2:
-            run_mode2()
-        elif RUN_MODE == 3:
-            run_mode3()
+    run_mode1()
 
 
 # creat a new canvas
-x = MiniGUI.Tablet(25, 100)
+canvas = MiniGUI.Tablet(1, 1)
 # run the program
 run()
