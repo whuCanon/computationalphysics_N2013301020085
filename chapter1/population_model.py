d9e2d85bef99b5b7b15d00a5cb2575cfb30c9039
whuCanon <liu136583410@gmail.com>
2016-03-19 13:32:08 +0800

    submit population_model.py

--- chapter1/population_model.py
+++ chapter1/population_model.py
@@ -4,13 +4,13 @@
 from pylab import *
 import math
 
-DET_T = 0.01
-a = 0.
-b = 0.
-end_t = 0
-N = []
-t = [0]
+DET_T = 0.01	# smaller the value, preciser the result
+a = 0. 			# the paradise a
+b = 0. 			# the paradise b
+N = []			# the list of number of population
+t = [0]			# the list of time moment
 
+# receive input paradise and calculate a suitable end time
 while True:
 	try:
 		a = float(raw_input("Please enter the parameter_a (best in 0.01-100):"))
@@ -25,19 +25,23 @@ while True:
 	except ValueError:
 		print "Please enter a right number!"
 
+# calculate the N every moment
 for i in range(int(end_t / DET_T)):
 	tmp_N = N[i] + (a * N[i] - b * N[i]**2) * DET_T
 	N.append(tmp_N)
 	t.append((i + 1) * DET_T)
 
+# get the minimal and maximal t and N
 xmin, xmax = min(t), max(t)
 ymin, ymax = int(min(N)), int(round(max(N)))
 
+# adjust the diagram
 dx = (xmax - xmin) * 0.1
 dy = (ymax - ymin) * 0.1
 xlim(xmin - dx, xmax + dx)
 ylim(ymin - dy, ymax + 2 * dy)
 
+# adjust the axis
 ax = gca()
 ax.spines['right'].set_color('none')
 ax.spines['top'].set_color('none')
@@ -46,16 +50,20 @@ ax.spines['bottom'].set_position(('data',0))
 ax.yaxis.set_ticks_position('left')
 ax.spines['left'].set_position(('data',0))
 
+# add label
 xticks([xmin, xmax])
 yticks([ymin, ymax/2, ymax],[ymin, ymax/2, ymax])
 
+# add auxiliary line
 t_p = ymax / 2	# turning point
 plot([0,xmax],[t_p,t_p], color='red', linewidth=2.5, linestyle="--")
 plot([0,xmax],[ymax,ymax], color='red', linewidth=2.5, linestyle="--")
 
+# name the axis
 xlabel('time/year')
 ylabel('population')
 
+# plot and save
 plot(t, N, label="population growth")
 legend(loc='upper center')
 savefig("exercice_2.png",dpi=256)
