8b72f84b8854faa899063cfd42f4438be1d1fbba
whuCanon <liu136583410@gmail.com>
2016-04-05 13:56:12 +0800

    添加动画效果

--- chapter2/Cannon.py
+++ chapter2/Cannon.py
@@ -1,9 +1,11 @@
 # a simple program to analyse cannon shell's trail
 # written by whuCanon, last modified on 2016/04/04
 
-from pylab import *
-import thread
+import sys
 import math
+import numpy 
+from matplotlib import pyplot 
+from matplotlib import animation
 
 # some arguments
 g = 9.8
@@ -11,11 +13,12 @@ B2m = 4e-5
 initSpeed = 1000.
 hit_area = 10.
 sita = math.pi / 2
+sign = 1
 
 
 class FlightState:
 
-    def __init__(self, _x=0, _y=0, _v_x=0, _v_y=0, _t=0):
+    def __init__(self, _x=0., _y=0., _v_x=0., _v_y=0., _t=0.):
         self.x  = _x
         self.y  = _y
         self.v_x = _v_x
@@ -56,74 +59,82 @@ class CannonShell:
         return factor
 
 
-def display(cannonShell):
-    # get the minimal and maximal value
-    x, y = [], []
-    for flightState in cannonShell.flightState:
-        x.append(flightState.x)
-        y.append(flightState.y)
-    xmin, xmax = min(x), max(x)
-    ymin, ymax = min(y), max(y)
-    # adjust the diagram
-    dy = (ymax - ymin) * 0.1
-    ylim(ymin, ymax + dy)
-    # add label
-    xticks([target[0]])
-    yticks([target[1]])
-    # add auxiliary line
-    plot([0,xmax],[target[1],target[1]], color='red', linewidth=2.5, linestyle="--")
-    # name the axis
-    xlabel(r'$x(m)$', fontsize=16)
-    ylabel(r'$y(m)$', fontsize=16)
-    # plot
-    plot(x, y, "blue", label="shootingTrail")
-    legend(loc='upper center')
-
-
 def calculate(target):
     initState = FlightState(0., 0., initSpeed * math.cos(sita), initSpeed * math.sin(sita))
     cannonShell = CannonShell(initState, 1)
     while True:
         if cannonShell.flightState[-1].y < target[1] and cannonShell.flightState[-1].v_y < 0:
+            dy = cannonShell.flightState[-1].y - target[1]
+            tanB = cannonShell.flightState[-1].v_x / cannonShell.flightState[-1].v_y
+            cannonShell.flightState[-1].x -= dy * tanB
+            cannonShell.flightState[-1].y = target[1]
             break
         cannonShell.getNextState()
     return cannonShell
 
 
 def storeData(target, accuracy):
-    #savefig("shootingTrail.png",dpi=256)
     f = open("shootParadise.txt", "w")
     print >> f, "Target position:\n\tX: %f \n\tY: %f \n" % (target[0], target[1])
-    print >> f, "Shooting angle: %f \nShooting Speed: %f\n" % (sita, initSpeed)
+    print >> f, "Shooting angle: %f \nShooting Speed: %f\n" % (sita * 180 / math.pi, initSpeed)
     print >> f, "Attacking accuracy: %f " % (accuracy)
     f.close()
 
 
 def shoot():
-    print "sita: ", sita
+    print "sita: ", sita * 180 / math.pi
     print "initSpeed: ", initSpeed
 
 
 farthest_x = 0
-target = [20000, 2000]
-#target.append(float(raw_input("Please input the target's X_position: ")))
-#target.append(float(raw_input("Please input the target's Y_position: ")))
+target = []
+target.append(float(raw_input("Please input the target's X_position: ")))
+target.append(float(raw_input("Please input the target's Y_position: ")))
 
-for i in range(100):
-    sita -= math.pi / (2 * 100)
+fig = pyplot.figure()
+xmin, xmax = 0., 4e+4
+ymin, ymax = 0., 1e+4
+dx = (xmax - xmin) * 0.1
+dy = (ymax - ymin) * 0.2
+ax = pyplot.axes(xlim=(xmin, xmax + dx), ylim=(ymin, ymax + dy))
+line, = ax.plot([], [])
+
+# add auxiliary line
+pyplot.plot([0,xmax],[target[1],target[1]], color='red', linewidth=2.5, linestyle="--")
+
+# name the axis
+pyplot.xlabel(r'$x(m)$', fontsize=16)
+pyplot.ylabel(r'$y(m)$', fontsize=16)
+
+# draw animation
+def initAnimation():   
+    line.set_data([], [])
+    return line,
+
+def animate(i):
+    global sign, sita, initSpeed, hit_area, farthest_x
+    x, y = [], []
     cannonShell = calculate(target)
-    #display(cannonShell)
-    if farthest_x < cannonShell.flightState[-1].x:
-        farthest_x = cannonShell.flightState[-1].x
+    for flightState in cannonShell.flightState:
+        x.append(flightState.x)
+        y.append(flightState.y)
+    if sign == 1:
+        sita -= math.pi / (2 * 100)
+        if farthest_x < cannonShell.flightState[-1].x:
+            farthest_x = cannonShell.flightState[-1].x
+        else:
+            sign = 2
+    elif sign == 2:
+        initSpeed -= 1
+        accuracy = math.fabs(cannonShell.flightState[-1].x - target[0])
+        if accuracy < hit_area:
+            shoot()
+            storeData(target, accuracy)
+            sign = 0
     else:
-        break
+    	sys.exit(0)
+    line.set_data(x, y)   
+    return line,
 
-for i in range(1000):
-    initSpeed -= 1
-    cannonShell = calculate(target)
-    #display(cannonShell)
-    accuracy = math.fabs(cannonShell.flightState[-1].x - target[0])
-    if accuracy < hit_area:
-        shoot()
-        storeData(target, accuracy)
-        break
+anim = animation.FuncAnimation(fig, animate, init_func=initAnimation, interval=20, blit=True)
+pyplot.show()
