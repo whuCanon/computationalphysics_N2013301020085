d26c65887db187e9cc6259f922a9a6e96b51c7f2
whuCanon <liu136583410@gmail.com>
2016-05-10 16:26:43 +0800

    fixed bug

--- chapter4/binaryStar.py
+++ chapter4/binaryStar.py
@@ -1,18 +1,18 @@
 import math
 from visual import *
 
-k = 2.
+k = 1.
 m = 100
 M = m * k
 d = 100.
 
 earth = sphere(pos=( d*M/(m+M),0,0), radius=5, color=color.white, material=materials.earth)
 sun   = sphere(pos=(-d*m/(m+M),0,0), radius=10, color=color.yellow, material=materials.emissive)
-earth.velocity = vector(0,math.sqrt(m*k**2/(k+1)/d),0)
-sun.velocity = vector(0,-math.sqrt(m*k/(k+1)/d),0)
-# both_vec = (k*sun.velocity + earth.velocity) / (1 + k)
-# earth.velocity -= both_vec
-# sun.velocity -= both_vec
+earth.velocity = vector(0,k*math.sqrt(m/(k+1)/d)/3,0)
+sun.velocity = vector(0,-math.sqrt(m/(k+1)/d),0)
+both_vec = (k*sun.velocity + earth.velocity) / (1 + k)
+earth.velocity -= both_vec
+sun.velocity -= both_vec
 earth.trail = curve(color=earth.color)
 sun.trail = curve(color=sun.color)
 
